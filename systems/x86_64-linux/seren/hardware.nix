ca482e5833bd57fa63a12f2839ab8f02b0f72a48
aemogie <54412618+aemogie@users.noreply.github.com>
2024-02-13 11:39:52 +0530

    feat(seren): update hardware using `nixos-generate-config`
    TODO: use NixOS/nixos-hardware

--- systems/x86_64-linux/seren/hardware.nix
+++ systems/x86_64-linux/seren/hardware.nix
@@ -1,21 +1,60 @@
-{modulesPath, ...}: {
+{
+  modulesPath,
+  lib,
+  config,
+  ...
+}: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
+  boot = {
+    kernelModules = ["kvm-intel"];
+
+    initrd = {
+      availableKernelModules = ["vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod"];
+      kernelModules = [];
+    };
+
+    extraModulePackages = [];
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "vfat";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+    };
+
+    "/home" = {
+      device = "/dev/disk/by-label/NIXHOME";
+      fsType = "ext4";
+    };
+
+    "/mnt/windows" = {
+      device = "/dev/disk/by-label/WINDOWS";
+      fsType = "ntfs-3g";
+      options = ["rw"];
+    };
+
+    "/mnt/windata" = {
+      device = "/dev/disk/by-label/WINDATA";
+      fsType = "ntfs-3g";
+      options = ["rw"];
+    };
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-label/swap";}
+    {device = "/dev/disk/by-label/SWAP";}
   ];
 
   networking.useDHCP = true;
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
