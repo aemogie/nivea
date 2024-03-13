156fa47da126c522baf9295a2b566f40ce7bc04d
aemogie <54412618+aemogie@users.noreply.github.com>
2024-03-13 08:14:56 +0530

    feat: experiment with aliases

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -1,26 +1,34 @@
 {
   lib,
   config,
+  options,
   ...
 }:
 with lib;
 with lib.nivea; let
   cfg = config.user;
+  opt = options.user;
 in {
   options.user = with types; {
+    enable = mkBoolOpt true "Whether to enable single-user configuration.";
     name = mkOpt str "aemogie" "The name to use for the user account.";
     initialPassword =
       mkOpt str "password"
       "The initial password to use when the user is first created.";
+    extraGroups = mkOpt (listOf str) [] "Extra groups to add the user to.";
   };
 
-  config.users.users.${cfg.name} = {
-    isNormalUser = true;
-    inherit (cfg) name initialPassword;
-    home = "/home/${cfg.name}";
-    group = "users";
-
+  config = mkIf cfg.enable {
     # no idea what half of these do, tbh
-    extraGroups = ["wheel" "audio" "video" "networkmanager" "input" "tty"];
+    user.extraGroups = ["wheel" "audio" "video" "networkmanager" "input" "tty"];
+
+    users.users.${cfg.name} = {
+      isNormalUser = true;
+      group = "users";
+      name = mkAliasDefinitions opt.name;
+      home = mkAliasAndWrapDefinitions (name: "/home/${name}") opt.name;
+      initialPassword = mkAliasDefinitions opt.initialPassword;
+      extraGroups = mkAliasDefinitions opt.extraGroups;
+    };
   };
 }
