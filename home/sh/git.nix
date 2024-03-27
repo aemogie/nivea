a6ad391476d30bff516ef82b63a0d46540a37229
aemogie <54412618+aemogie@users.noreply.github.com>
2024-03-27 21:00:54 +0530

    refactor: git to new file

---
+++ home/sh/git.nix
@@ -0,0 +1,24 @@
+{
+  programs = {
+    git = {
+      enable = true;
+      userName = "aemogie";
+      userEmail = "54412618+aemogie@users.noreply.github.com";
+      extraConfig = {
+        init.defaultBranch = "dev";
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+        user.signingkey = "~/.ssh/id_ed25519.pub";
+      };
+    };
+    gh = {
+      enable = true;
+      settings.version = 1; # nix-community/home-manager#4744
+      settings.git_protocol = "ssh";
+      gitCredentialHelper.hosts = [
+        "https://github.com"
+        "https://gist.github.com"
+      ];
+    };
+  };
+}
--- home/user/aemogie.nix
+++ home/user/aemogie.nix
@@ -20,6 +20,7 @@
       ../wm/swaylock.nix
     ]
     ++ [
+      ../sh/git.nix
       ../sh/helix
       ../sh/bat.nix
       ../sh/nushell
@@ -81,26 +82,6 @@
       };
     };
 
-    git = {
-      enable = true;
-      userName = "aemogie";
-      userEmail = "54412618+aemogie@users.noreply.github.com";
-      extraConfig = {
-        init.defaultBranch = "dev";
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        user.signingkey = "~/.ssh/id_ed25519.pub";
-      };
-    };
-    gh = {
-      enable = true;
-      settings.version = 1; # nix-community/home-manager#4744
-      settings.git_protocol = "ssh";
-      gitCredentialHelper.hosts = [
-        "https://github.com"
-        "https://gist.github.com"
-      ];
-    };
     # obs-studio = {
     #   enable = false;
     #   plugins = with pkgs.obs-studio-plugins; [
