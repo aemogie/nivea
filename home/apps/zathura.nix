33d2377448466ff7092706c378e48401b6368f5c
aemogie <54412618+aemogie@users.noreply.github.com>
2024-05-03 23:30:50 +0530

    feat(zathura): translucent selection

--- home/apps/zathura.nix
+++ home/apps/zathura.nix
@@ -10,45 +10,50 @@ let
     primary
     alternate
     ;
-  theme = {
-    default-fg = "#${text}";
-    default-bg = "#${base}";
+  theme =
+    builtins.mapAttrs
+      (_: c: "rgba(${toString c.r}, ${toString c.g}, ${toString c.b}, ${toString (c.a or 1)})")
+      {
+        default-fg = text;
+        default-bg = base;
 
-    completion-bg = "#${surface0}";
-    completion-fg = "#${text}";
-    completion-highlight-bg = "#${surface2}";
-    completion-highlight-fg = "#${text}";
-    completion-group-bg = "#${surface0}";
-    completion-group-fg = "#${alternate}";
+        completion-bg = surface0;
+        completion-fg = text;
+        completion-highlight-bg = surface2;
+        completion-highlight-fg = text;
+        completion-group-bg = surface0;
+        completion-group-fg = alternate;
 
-    statusbar-fg = "#${text}";
-    statusbar-bg = "#${surface0}";
+        statusbar-fg = text;
+        statusbar-bg = surface0;
 
-    notification-bg = "#${surface0}";
-    notification-fg = "#${text}";
-    notification-error-bg = "#${surface0}";
-    notification-error-fg = "#${error}";
-    notification-warning-bg = "#${surface0}";
-    notification-warning-fg = "#${warning}";
+        notification-bg = surface0;
+        notification-fg = text;
+        notification-error-bg = surface0;
+        notification-error-fg = error;
+        notification-warning-bg = surface0;
+        notification-warning-fg = warning;
 
-    inputbar-fg = "#${text}";
-    inputbar-bg = "#${surface0}";
+        inputbar-fg = text;
+        inputbar-bg = surface0;
 
-    recolor-lightcolor = "#${base}";
-    recolor-darkcolor = "#${text}";
+        recolor-lightcolor = base;
+        recolor-darkcolor = text;
 
-    index-fg = "#${text}";
-    index-bg = "#${base}";
-    index-active-fg = "#${text}";
-    index-active-bg = "#${surface0}";
+        index-fg = text;
+        index-bg = base;
+        index-active-fg = text;
+        index-active-bg = surface0;
 
-    render-loading-bg = "#${base}";
-    render-loading-fg = "#${text}";
+        render-loading-bg = base;
+        render-loading-fg = text;
 
-    highlight-color = "#${primary}";
-    highlight-fg = "#${text}";
-    highlight-active-color = "#${text}";
-  };
+        highlight-color = primary // {
+          a = 0.5;
+        };
+        highlight-fg = text;
+        highlight-active-color = text;
+      };
 in
 {
   programs.zathura = {
