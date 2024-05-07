fef7d7ddfcefc268ece1585da42c4131e8da707c
aemogie <54412618+aemogie@users.noreply.github.com>
2024-05-08 01:47:11 +0530

    feat: try out lix

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
     {
       nixpkgs,
       home-manager,
-      hyprland,
-      self,
+      lix-module,
       ...
     }@inputs:
     let
@@ -21,6 +20,7 @@
         };
         modules = [
           home-manager.nixosModules.home-manager
+          lix-module.nixosModules.default
           {
             networking.hostName = hostName;
             home-manager.extraSpecialArgs = specialArgs;
@@ -41,12 +41,14 @@
       "https://hyprland.cachix.org"
       "https://anyrun.cachix.org"
       "https://nix-community.cachix.org"
+      "https://cache.lix.systems"
     ];
     extra-trusted-public-keys = [
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
     ];
   };
 
@@ -54,6 +56,15 @@
     nixpkgs.url = "github:auxolotl/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    lix = {
+      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+      flake = false;
+    };
+    lix-module = {
+      url = "git+https://git.lix.systems/lix-project/nixos-module";
+      inputs.lix.follows = "lix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
