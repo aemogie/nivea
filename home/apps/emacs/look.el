dcc534eaeb04fd648558946a6167f0f8527955d5
aemogie <54412618+aemogie@users.noreply.github.com>
2024-05-19 01:19:50 +0530

    chore(emacs): decrease font size

--- home/apps/emacs/look.el
+++ home/apps/emacs/look.el
@@ -3,7 +3,7 @@
 (tooltip-mode -1)
 (tool-bar-mode -1)
 
-(add-to-list 'default-frame-alist '(font . "Iosevka 14"))
+(add-to-list 'default-frame-alist '(font . "Iosevka 13"))
 (add-to-list 'default-frame-alist '(alpha-background . 80))
 (pixel-scroll-precision-mode)
 
