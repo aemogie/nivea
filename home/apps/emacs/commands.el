79eda9112ed7e7c30d6425ee7a441ec8cac4cba9
aemogie <54412618+aemogie@users.noreply.github.com>
2024-10-12 21:46:02 +0530

    refactor(emacs): better ux for bluetooth-connect

--- home/apps/emacs/commands.el
+++ home/apps/emacs/commands.el
@@ -1,16 +1,22 @@
 (defun bluetooth-connect ()
-  "Connect to bluetooth device"
+  "Connect to a bluetooth device"
   (interactive)
   (let* ((devices  (split-string (shell-command-to-string "bluetoothctl devices") "\n" t))
-         (selected (completing-read "Select Bluetooth device: " devices))
-         (mac      (cadr (split-string selected " "))))
+	 (choices  (mapcar (lambda (device)
+			     (let* ((parts (split-string device " "))
+				    (mac (cadr parts))
+				    (name (string-join (cddr parts) " ")))
+			       (cons name mac)))
+			   devices))
+         (selected (completing-read "Select Bluetooth device: " (mapcar #'car choices)))
+         (mac      (cdr (assoc selected choices))))
     (when mac
-      (message "Attempting to connect to %s" selected)
+      (message "Attempting to connect to %s (%s)" selected mac)
       (make-process
        :name "bluetoothctl-connect"
        :buffer "*bluetoothctl-output*"
        :command (list "bluetoothctl" "connect" mac)
        :sentinel (lambda (process event)
                    (if (string= event "finished\n")
-                       (message "Successfully connected to %s" selected)
-		     (message "Error connecting to %s" selected)))))))
+                       (message "Successfully connected to %s (%s)" selected mac)
+		     (message "Error connecting to %s (%s)" selected mac)))))))
