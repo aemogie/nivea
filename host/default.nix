be497aeb9e78c7c9d80780113537fe77432fef82
aemogie <theaemogie@gmail.com>
2025-03-31 18:55:28 +0530

    fix: mic input is broken on pipewire

--- host/default.nix
+++ host/default.nix
@@ -100,8 +100,9 @@
   };
 
   # Enable sound with pipewire.
+  hardware.pulseaudio.enable = true;
   services.pipewire = {
-    enable = true;
+    enable = false;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
