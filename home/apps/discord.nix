c83e195045ae1f2b52ac680143908198793c0941
aemogie <theaemogie@gmail.com>
2025-12-06 18:36:25 +0530

    discord ctp theme

--- home/apps/discord.nix
+++ home/apps/discord.nix
@@ -1,6 +1,36 @@
+{ pkgs, osConfig, ... }:
 {
   programs.vesktop = {
     enable = true;
-    vencord.useSystem = true;
+    settings = {
+      enableSplashScreen = true;
+      splashTheming = true;
+      splashBackground = "#${osConfig.paint.active.palette.base}";
+      splashColor = "#${osConfig.paint.active.palette.text}";
+      splashPixelated = true;
+      enabledThemes = [ "catppuccin.css" ];
+    };
+    vencord = {
+      useSystem = true;
+      themes =
+        let
+          darkUrl =
+            let
+              inherit (osConfig.paint.dark.ctpCompat) flavor accent;
+            in
+            "https://catppuccin.github.io/discord/dist/catppuccin-${flavor}-${accent}.theme.css";
+          lightUrl =
+            let
+              inherit (osConfig.paint.light.ctpCompat) flavor accent;
+            in
+            "https://catppuccin.github.io/discord/dist/catppuccin-${flavor}-${accent}.theme.css";
+        in
+        {
+          catppuccin = pkgs.writeText "ctpCompat.css" ''
+            @import url("${darkUrl}") (prefers-color-scheme: dark);
+            @import url("${lightUrl}") (prefers-color-scheme: light);
+          '';
+        };
+    };
   };
 }
